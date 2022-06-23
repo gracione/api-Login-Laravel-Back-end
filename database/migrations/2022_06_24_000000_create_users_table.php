49edf5f8ada56a2e01392888b5181fc595b867bf
gracione <gracione2017@gmail.com>
2022-06-23 20:59:46 -0300

    atualizando

---
+++ database/migrations/2022_06_24_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('numero');
+            $table->integer('bo_funcionario');
+            $table->integer('id_sexo')->unsigned();
+            $table->foreign('id_sexo')->references('id')->on('sexo');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
+Route::post('/registrar', [App\Http\Controllers\API\AuthController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
