0e42def1070291801227681a2124b2280353f56e
gracione <gracione2017@gmail.com>
2022-09-14 06:21:15 -0300

    listar profissao

--- app/Models/Profissao.php
+++ app/Models/Profissao.php
@@ -10,14 +10,17 @@ class Profissao extends Model
 {
     use HasFactory;
 
-    public function listar ($request) {
+    public function listar($request)
+    {
         $select = DB::table('profissao')
-        ->select('*')
-        ->get();
+            ->select('*')
+            ->where('id_estabelecimento', $request->id_estabelecimento)
+            ->get();
         return $select;
     }
-    public function inserir ($request) {
-         DB::table('profissao')->insert([
+    public function inserir($request)
+    {
+        DB::table('profissao')->insert([
             'nome' => $request->nome,
             'id_estabelecimento' => $request->id_estabelecimento
         ]);
