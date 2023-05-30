921a8bcf698dbcbee8b23c690fd3dd643f58bf2d
gracione <gracione2017@gmail.com>
2023-05-30 06:45:50 -0300

    adicionando tabela de mensagens

---
+++ database/migrations/2023_05_30_093949_create_mensagens_table.php
@@ -0,0 +1,36 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMensagensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mensagens', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('remetente_id');
+            $table->unsignedBigInteger('destinatario_id');
+            $table->text('conteudo');
+            $table->timestamps();
+
+            $table->foreign('remetente_id')->references('id')->on('users');
+            $table->foreign('destinatario_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mensagens');
+    }
+}
